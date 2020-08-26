8d9440b3ddc2e5d0afa88fdde22bc517fe9630e6
Sany Mohammad Khaled <sany.khaled@northsouth.edu>
2020-08-26 17:45:54 +0600

    Numbers,String,Arrays

---
+++ Bohubrihi/JavaScript/Js/arrays.js
@@ -0,0 +1,43 @@
+var arr = ['Sany', 'Turza', 'Rhythm', 'Shakil']
+
+console.log(typeof(arr))
+document.getElementById("array").innerHTML = arr[2]
+
+arr[4] = 'Joy'
+
+console.log(arr[4])
+
+arr.push('John')
+
+console.log(arr)
+
+arr.pop(arr[2])
+
+console.log(arr)
+
+arr.shift()
+
+console.log(arr)
+
+arr.unshift('Friends')
+
+console.log(arr)
+
+var x = "I love coding"
+
+console.log(x.split(" "))
+
+var y = "Bangladesh,China,India,Canada,Pakistan" //String to array
+
+console.log(y.split(","))
+
+z= y.split(",")
+
+console.log(typeof(z))
+
+i = z.toString();
+
+console.log(arr.sort())
+
+console.log(arr.reverse())
+
---
+++ Bohubrihi/JavaScript/Js/string.js
@@ -0,0 +1,10 @@
+var x = '  saNy mohamMad khAleD'
+
+console.log(x.length)
+console.log(x.toLowerCase())
+console.log(x.toUpperCase())
+console.log(x[9])
+
+console.log(x.trim())
+console.log(x.slice(4,9))
+console.log(x.substr(6,6))
